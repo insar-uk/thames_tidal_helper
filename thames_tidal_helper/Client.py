879b62b408693f8125359d0423298b4fb91bfb3c
SA <saa116@ic.ac.uk>
2024-10-21 18:48:14 +0100

    Add tidal sites. Small refactor.

--- thames_tidal_helper/Client.py
+++ thames_tidal_helper/Client.py
@@ -8,18 +8,33 @@ import requests as req
 class API:
     root = "https://tidepredictions.pla.co.uk/gauge_data/"
 
-    site_codes = {
-        "Chelsea Bridge": "0113A",
-        "Richmond": "0113",
+    class TidalGauge:
+        def __init__(self, name: str, code: str):
+            self.name = name
+            self.code = code
+
+    TIDAL_GAUGES = {
+        TidalGauge("Margate", "0103"),
+        TidalGauge("Southend", "0110"),
+        TidalGauge("Coryton", "0110A"),
+        TidalGauge("Tilbury", "0111"),
+        TidalGauge("North Woolwich", "0112"),
+        TidalGauge("London Bridge", "0113"),
+        TidalGauge("Chelsea Bridge", "0113A"),
+        TidalGauge("Richmond", "0116"),
+        TidalGauge("Shivering Sand", "0116A"),
+        TidalGauge("Walton on the Naze", "0129"),
     }
 
     @staticmethod
-    def query(site: str, year: int, month: int, day: int):
+    def query_url(site: str, year: int, month: int, day: int):
+        """Return the URL to query the API for a specific date and named site"""
         try:
-            site_code = API.site_codes[site]
+            gauge = next(gauge for gauge in API.TIDAL_GAUGES if gauge.name == site)
+            site_code = gauge.code
         except KeyError:
             raise ValueError(
-                f"Site code for {site} not found. Please add it to the API.site_codes dictionary."
+                f"Site code for {site} not found. Please add it to the API.SITES list."
             )
 
         return f"{API.root}{site_code}/{year}/{month}/{day}/0/1/"
@@ -171,7 +186,9 @@ def get_quarters(
             continue
         # Query the API
         response = req.get(
-            API.query(site, quarter.year, quarter_to_month(quarter.quarter) * 3 - 2, 1)
+            API.query_url(
+                site, quarter.year, quarter_to_month(quarter.quarter) * 3 - 2, 1
+            )
         )
         cache.write_to_cache(quarter, response.text)
 
